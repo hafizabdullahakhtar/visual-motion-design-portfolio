f849b67c4ba3c52332a2904726b860d331338b7e
hafizabdullahakhtar <abdullah.akhtar@consforc.com>
2025-04-15 06:33:15 +0500

    Removed .js version, kept .jsx

--- src/PAGES/aboutme_page/animationSettings.js
+++
@@ -1,8 +0,0 @@
-export const animationSettings = {
-  container: { type: "spring", stiffness: 300, damping: 20 },
-  avatar: { type: "spring", stiffness: 300, damping: 20, duration: 3 },
-  text: { duration: 0.1 },
-  circles: { duration: 0.5, type: "spring", stiffness: 300, damping: 20 },
-  modalContainer: { type: "spring", stiffness: 300, damping: 20 },
-  nospring: { type: "tween", stiffness: 300, damping: 20 },
-};
